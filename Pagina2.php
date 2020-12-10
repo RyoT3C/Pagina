2aaaf8d8a6b4a874bb3fde1ffce23938b9ff3027
Manuel Mejia <48103832+RyoT3C@users.noreply.github.com>
2020-12-10 10:40:37 -0600

    Update Pagina2.php

--- Pagina2.php
+++ Pagina2.php
@@ -2,5 +2,7 @@
 $nombre = $_POST["nombre"]; 
 $apellido = $_POST["apellido"]; 
 $email = $_POST["email"]; 
-echo "Hola " . $nombre . " " . $apellido . ", Te has registrado con el siguiente correo electrónico: " . $email . "."; 
+echo "Hola " , $nombre , " " , $apellido;
+echo "<br>"; echo "<hr>";
+echo "Te has registrado con el siguiente correo electrónico: " , $email; 
 ?>
